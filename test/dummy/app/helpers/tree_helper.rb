aac0ed5a13853bb67994e38675e7f804291147c5
nirname <magistrgloom@gmail.com>
2013-04-17 19:01:47 +0400

    Fix css first-child

--- test/dummy/app/helpers/tree_helper.rb
+++ test/dummy/app/helpers/tree_helper.rb
@@ -3,14 +3,13 @@ module TreeHelper
   def draw_heirarcy(path = "")
     raw(Dir[Rails.root.join(path).to_s + "/*"].sort.group_by{ |content| File.directory?(content) }.instance_eval{ |h| [h[true], h[false]] }.compact.flatten.map do |content|
       name = Pathname.new(content).basename.to_s
-      title = name +
-        content_tag(:div, class: "btn-group btn-group-tree") do
-          content_tag(:div, class: "btn") do
-            content_tag(:i, nil, class: "icon-list")
-          end +
-          content_tag(:div, class: "btn") do
-            content_tag(:i, nil, class: "icon-th")
-          end
+
+      title = link_to(name, "#") +
+        content_tag(:div, class: "btn btn-tree") do
+          content_tag(:i, nil, class: "icon-list")
+        end +
+        content_tag(:div, class: "btn btn-tree") do
+          content_tag(:i, nil, class: "icon-th")
         end
       if File.directory?(content)
         simple_node title do
